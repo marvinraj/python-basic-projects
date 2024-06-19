4410b449abf546cb67b4fc199c79e0f6bf2a07b3
Marvin <marvinraj77@gmail.com>
2024-06-19 22:43:25 +0800

    created files and ideas

---
+++ contact book/main.py
@@ -0,0 +1,11 @@
+contact = {
+    "marvin": "0164193317",
+    "divya": "0164642422"
+}
+
+while True:
+    choices = input("\nwhat u want do?\n1. search contact\n2. add contact\n3. delete contact\n4. view contact list\n")
+    break
+
+if __name__ == "__main__":
+    pass
